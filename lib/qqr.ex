217ac8dac2efbc6160be89b7a8537a9f839329b7
Danila Poyarkov <dev@dannote.net>
2026-03-27 17:01:53 +0300

    Initial commit

---
+++ lib/qqr.ex
@@ -0,0 +1,18 @@
+defmodule QQR do
+  @moduledoc """
+  Documentation for `QQR`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> QQR.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/qqr_test.exs
@@ -0,0 +1,8 @@
+defmodule QQRTest do
+  use ExUnit.Case
+  doctest QQR
+
+  test "greets the world" do
+    assert QQR.hello() == :world
+  end
+end
